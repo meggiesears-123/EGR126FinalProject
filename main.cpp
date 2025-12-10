67e69af76886f52a6a4394ff9f220f0b29b79bbe
Meggie Sears <meggiesears@Meggies-MacBook-Air.local>
2025-12-10 11:45:24 -0500

    Best attempt so far. Everything complies. Functions don't work

--- main.cpp
+++ main.cpp
@@ -10,13 +10,19 @@ struct Letters{
     bool guessed; 
 };
 
-//Function to remove what letter are able to be guessed 
-string availbeLetter (string guess);
+bool isGameDone (Letters phrase [15]);
+bool didTheyGuessRight(string guess);
+
+string allLetters[26] = {" a"," b"," c"," d"," e"," f"," g"," h"," i"," j"," k"," l"," m"," n"," o"," p"," q"," r"," s"," t"," u"," v"," w"," x"," y"," z"}; 
+string guessedLetters[26] = {" #", " #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #"," #",};
+string printGuessedLetter = "";  
+
 
 int main()
 {
     string guess = " "; 
     bool gameFinished = false;
+    bool didTheyWin = false;
     
     //Hard coding phrase
     Letters phrase [15];
@@ -35,6 +41,7 @@ int main()
     phrase[12] = {"*", "e" , false};
     phrase[13] = {"*", "a" , false};
     phrase[14] = {"*", "t" , false};
+  
     
     // Printing initial display  
     cout << "INPUT A LETTER TO BEGIN PLAYING. YOU HAVE 16 GUESSES!" << endl;
@@ -49,16 +56,22 @@ int main()
     }
     cout << endl << "---------------------------------" << endl;
     
-    cout << "Avalible Letters:" << availbeLetter (guess) << endl;
+    cout << "Avalible Letters:" ;
+    for (int i = 0; i < 26; i++){
+        cout << allLetters[i];
+    }
 
+    // game loop
     while (gameFinished == false){ 
         
      int gameLength = 16;
+     
         for (int i = 0; i < gameLength; i++){ 
-            
-         cout << "Letter guess: "; 
+           
+            // cin-ing guess
+            cout << endl << "Letter guess: "; 
             cin >> guess; 
-         cout << endl;
+            cout << endl;
             
          //Finding and changing matching letters
             for (int i = 0; i < 15; i++){
@@ -67,15 +80,31 @@ int main()
                 }
             }
             
+            //Saving guessed letters
+            guess = " " + guess;
+            
+            for ( int i = 0; i < 26; i++){
+               
+                if (guess == allLetters[i]){
+                    
+                    cout << "Good Guess! " << endl;
+                    guessedLetters[i] = allLetters[i];
+                    allLetters[i] = " #";
+                }
+            }
+            
             //Provide feedback 
-            if (guess == phrase[1].realChar || guess == phrase[2].realChar || guess == phrase[3].realChar || guess == phrase[4].realChar ||
-            guess == phrase[5].realChar || guess == phrase[6].realChar || guess == phrase[7].realChar || guess == phrase[8].realChar ||
-            guess == phrase[9].realChar || guess == phrase[10].realChar || guess == phrase[11].realChar || guess == phrase[12].realChar || 
-            guess == phrase[13].realChar || guess == phrase[14].realChar || guess == phrase[0].realChar){ 
-                
+            bool didTheyGuessRightBool = didTheyGuessRight(guess);
+            
+            if (didTheyGuessRightBool == true){
                 cout << "Good Guess!" << endl;
             }
-            else cout << "Try agian :(" << endl;
+            
+            else cout << "Try again :(" << endl;
+            
+            //Guesses Left 
+            cout << "Guesses Remaining: " << gameLength - i - 1 << endl;
+            
             
             //Printing new board 
             cout << "---------------------------------" << endl << "|   | ";
@@ -89,54 +118,78 @@ int main()
             cout << endl << "---------------------------------" << endl;
             cout << endl;
             
-            cout << "Avalible Letters:" << availbeLetter (guess) << endl;
             
-            //Checking to see it game is done
-            if (phrase[0].realChar == phrase[0].shownChar && phrase[1].realChar == phrase[1].shownChar && phrase[2].realChar == phrase[2].shownChar && 
-            phrase[3].realChar == phrase[3].shownChar && phrase[4].realChar == phrase[4].shownChar && phrase[5].realChar == phrase[5].shownChar && 
-            phrase[6].realChar == phrase[6].shownChar && phrase[7].realChar == phrase[7].shownChar && phrase[8].realChar == phrase[8].shownChar && 
-            phrase[9].realChar == phrase[9].shownChar && phrase[10].realChar == phrase[10].shownChar && phrase[11].realChar == phrase[11].shownChar && 
-            phrase[12].realChar == phrase[12].shownChar && phrase[13].realChar == phrase[13].shownChar && phrase[14].realChar == phrase[14].shownChar){
+            printGuessedLetter += guess;
+            cout << "Guessed Letters:" << printGuessedLetter << endl;
+            
+            cout << "Avalible Letters:";
+            for (int i = 0; i < 26; i++){
                 
-                gameFinished = true;
-                cout << "YOU WON!";
+                if (allLetters[i] != " #"){
+                    
+                    cout << allLetters[i];
+                }
             }
             
-            cout << "YOU LOST"; 
-            gameFinished = true;
+            gameFinished = isGameDone;
+            
+            if (gameFinished == true){
+                didTheyWin = true;
+            }
         }
-        
     }
 
-return 0;
-    
+    if (didTheyWin == false) {
+        
+        cout << "YOU LOST";
+    }
+
+    else cout << "YOU WON";
+
+    return 0;
 }
 
-string availbeLetter (string guess){ 
+bool didTheyGuessRight(string guess){
     
-    string lettersArray [26] = {" a"," b"," c"," d"," e"," f"," g"," h"," i"," j"," k"," l"," m"," n"," o"," p"," q"," r"," s"," t"," u"," v"," w"," x"," y"," z"}; 
+    Letters phrase [15];
+    phrase[0] = {"*", "c" , false};
+    phrase[1] = {"*", "o" , false};
+    phrase[2] = {"*", "d" , false};
+    phrase[3] = {"*", "i" , false};
+    phrase[4] = {"*", "n" , false};
+    phrase[5] = {"*", "g" , false};
+    phrase[6] = {" ", " " , false};
+    phrase[7] = {"*", "i" , false};
+    phrase[8] = {"*", "s" , false};
+    phrase[9] = {" ", " " , false};
+    phrase[10] = {"*", "g" , false};
+    phrase[11] = {"*", "r" , false};
+    phrase[12] = {"*", "e" , false};
+    phrase[13] = {"*", "a" , false};
+    phrase[14] = {"*", "t" , false};
     
-    guess = " " + guess;
-    
-    for ( int i = 0; i < 26; i++){
+    if (guess == phrase[1].realChar || guess == phrase[2].realChar || guess == phrase[3].realChar || guess == phrase[4].realChar ||
+    guess == phrase[5].realChar || guess == phrase[6].realChar || guess == phrase[7].realChar || guess == phrase[8].realChar ||
+    guess == phrase[9].realChar || guess == phrase[10].realChar || guess == phrase[11].realChar || guess == phrase[12].realChar || 
+    guess == phrase[13].realChar || guess == phrase[14].realChar || guess == phrase[0].realChar){ 
         
-        if (guess == lettersArray[i]){
-            
-            lettersArray[i] = "#";
-        }
+        return true;
     }
     
-    string returnString; 
+    else return false;
+}
+
+bool isGameDone (Letters phrase [15]){
     
-    for (int i = 0; i < 26; i++){ 
+    if (phrase[0].realChar == phrase[0].shownChar && phrase[1].realChar == phrase[1].shownChar && phrase[2].realChar == phrase[2].shownChar && 
+    phrase[3].realChar == phrase[3].shownChar && phrase[4].realChar == phrase[4].shownChar && phrase[5].realChar == phrase[5].shownChar && 
+    phrase[6].realChar == phrase[6].shownChar && phrase[7].realChar == phrase[7].shownChar && phrase[8].realChar == phrase[8].shownChar && 
+    phrase[9].realChar == phrase[9].shownChar && phrase[10].realChar == phrase[10].shownChar && phrase[11].realChar == phrase[11].shownChar && 
+    phrase[12].realChar == phrase[12].shownChar && phrase[13].realChar == phrase[13].shownChar && phrase[14].realChar == phrase[14].shownChar){
         
-        if (lettersArray[i] != "#"){
-            
-            returnString +=lettersArray[i]; 
-        }
+        return true;
+        
     }
-
-return returnString;
-} 
-
-
+    
+    else return false;
+}
